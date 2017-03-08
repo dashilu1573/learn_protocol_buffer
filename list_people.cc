7b90381391313f18a2d45538efdc04283b23e8b1
luyafei <luyafei@weidian.com>
2017-03-08 14:48:17 +0800

    add learn_protocal_buffer

---
+++ list_people.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "addressbook.pb.h"
+
+using namespace std;
+
+// Iterates though all people in the AddressBook and prints info about them.
+void ListPeople(const tutorial::AddressBook& address_book) {
+    for (int i = 0; i < address_book; ++i) {
+        const tutorial::Person& person = address_book.people(i);
+
+        cout << "Person ID: " << person.id() << endl;
+        cout << " Name: " << person.name() << end;
+        if (person.has_email()) {
+            cout << "E-mail address: " << person.email() << endl;
+        }
+
+        for (int j = 0; j < person.phones_size(); j++) {
+            const tutorial::Person::PhoneNumber& phone_number = person.phones(j);
+
+            switch (phone_number.type()) {
+                case tutorial::Person::MOBILE:
+                    cout << " Mobile phone #: ";
+                    break;
+                case tutorial::Person::HOME:
+                    cout << "  Home phone #: ";
+                    break;
+                case tutorial::Person::WORK:
+                    cout << "  Work phone #: ";
+                    break;
+            }
+            cout << phone_number.number() << endl;
+        }
+    }
+}
+
+//Main function: Read the entire address book from a file and prints all the information inside.
+int main(int args, char* argv[]) {
+    // Verify that the version of the library that we linked against is
+    // compatible with the version of the headers we compiled against.
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    if (argc != 2) {
+        cerr << "Usage:  " << argv[0] << " ADDRESS_BOOK_FILE" << endl;
+        return -1;
+    }
+
+    tutorial::AddressBook addressBook;
+
+    {
+        // Read the existing address book.
+        fstream input(argv[1], ios::in | ios::binary);
+        if (! addressBook.ParseFromIstream(&input)) {
+            cerr << "Failed to parse address book." << endl;
+            return -1;
+        }
+    }
+
+    ListPeople(addressBook);
+
+    // Optional: Delete all global objects allocated by libprotobuf.
+    google::protobuf::ShutdownProtobufLibrary();
+
+    return 0;
+}
